82fe0059e77b3959dc1984c61c51b83c65c04e4d
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-04-16 19:47:59 -0400

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  addingEventListeners();
+ 
 });
+
+
+
+//Get the form and adding EventListner
+
+
+function addingEventListeners()
+{
+document.getElementById("create-task-form").addEventListener("submit",handleFormSubmit);
+  
+}
+
+
+
+function handleFormSubmit(e) {
+  e.preventDefault()
+ const task = e.target[0].value;
+ displayTask(task);
+  
+}
+
+function displayTask(task) {
+
+const taskUl= document.getElementById("tasks")
+const taskLi=document.createElement("li");
+const deleteBtn=document.createElement("button");
+deleteBtn.textContent="x";
+deleteBtn.addEventListener("click",deleteTask)
+taskLi.textContent=task + "  ";
+taskLi.appendChild(deleteBtn);
+taskUl.appendChild(taskLi)
+
+}
+
+
+
+function deleteTask(e) {
+e.target.parentNode.remove();
+}
+
